caf2ab7213037c6401bf478e6051273cf2b4939e
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-07-14 15:56:53 +0300

    Создан тестовый класс, проверяющий правильность вычисления расстояния
    + добавлены зависимости в build.gradle

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+    @Test
+    public void testDistance(){
+        /*1я проверка*/
+        Point p1 = new Point(12, 2);
+        Point p2 = new Point(2, 54);
+        Assert.assertEquals(p1.distance(p2), 52.952809179494906);
+    }
+}
