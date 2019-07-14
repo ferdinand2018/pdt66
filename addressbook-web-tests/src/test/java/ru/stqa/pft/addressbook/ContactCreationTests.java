f412939e689a777828030993e62ec773b5de6e14
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-07-14 18:02:08 +0300

    Контакты: Параметризован метод заполнения формы.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,23 +13,23 @@ public class ContactCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testContactCreationTests() throws Exception {
     gotoContactCreationPage(); //Переход на страницу создания контактов
-    fillContactForm(); //Заполнение данных о контакте
+    fillContactForm("firstname", "middlename", "lastname", "123", "321"); //Заполнение данных о контакте
     submitContactCreation(); //Отправка данных
     returnToContactPage(); //Переход на страницу контактов
     logout(); //Выход из системы
@@ -48,19 +47,19 @@ public class ContactCreationTests {
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(String firstname, String middlename, String lastname, String mobile, String work) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("firstname");
+    wd.findElement(By.name("firstname")).sendKeys(firstname);
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("middlename");
+    wd.findElement(By.name("middlename")).sendKeys(middlename);
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("lastname");
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("123");
+    wd.findElement(By.name("mobile")).sendKeys(mobile);
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("321");
+    wd.findElement(By.name("work")).sendKeys(work);
   }
 
   private void gotoContactCreationPage() {
