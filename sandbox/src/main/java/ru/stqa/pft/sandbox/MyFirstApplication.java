d63e4ead389d0cd5c76c2f42553ae6f5c5bf27cb
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-07-14 15:01:43 +0300

    Добавлен пакет

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
@@ -1,4 +1,6 @@
-public class MyFirstApplication{	
+package ru.stqa.pft.sandbox;
+
+public class MyFirstApplication{
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}	
