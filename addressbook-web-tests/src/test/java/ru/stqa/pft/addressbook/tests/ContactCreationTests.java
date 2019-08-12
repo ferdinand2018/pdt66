d80a5d72e6bcdc9e9bf98b2b7dce83d8fe15ba4a
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-08-12 20:34:06 +0300

    Оптимизация заполнения форм: оставляем дефолтные значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoContactCreationPage(); //Переход на страницу создания контактов
-    app.getContactHelper().fillContactForm(new ContactData("firstname", "middlename", "lastname", "123", "321")); //Заполнение данных о контакте
+    app.getContactHelper().fillContactForm(new ContactData("firstname", "middlename", "lastname", "123", null)); //Заполнение данных о контакте
     app.getContactHelper().submitContactCreation(); //Отправка данных
     app.getContactHelper().returnToContactPage(); //Переход на страницу контактов
     app.logout(); //Выход из системы
