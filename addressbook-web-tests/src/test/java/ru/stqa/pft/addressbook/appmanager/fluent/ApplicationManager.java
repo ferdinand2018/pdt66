2d89138a5f8a0149b5ce451ab771141dd6233ef8
ferdinand2018 <40237280+ferdinand2018@users.noreply.github.com>
2019-09-23 19:14:30 +0300

    Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/fluent/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/fluent/ApplicationManager.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.appmanager.fluent.ContactHelper;
@@ -12,9 +13,15 @@ import ru.stqa.pft.addressbook.appmanager.fluent.GroupHelper;
 import ru.stqa.pft.addressbook.appmanager.fluent.NavigationHelper;
 import ru.stqa.pft.addressbook.appmanager.SessionHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
@@ -24,51 +31,40 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if(browser.equals(BrowserType.FIREFOX)){
-            wd = new FirefoxDriver();
+            //wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files (x86)/Mozilla Firefox ESR/firefox.exe"));
         } else if(browser.equals(BrowserType.CHROME)){
             wd = new ChromeDriver();
         } else if(browser.equals(BrowserType.IE)){
             wd = new InternetExplorerDriver();
         }
-
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
-    }
-
-    public void logout() {
-      wd.findElement(By.linkText("Logout")).click();
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
         wd.quit();
     }
 
-    private boolean isElementPresent(By by) {
-      try {
-        wd.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
-
-
-
-    public ContactHelper contact(){
-        return contactHelper;
-    }
-
     public GroupHelper group() {
         return groupHelper;
+    }
+
+    public ContactHelper contact() {
+        return contactHelper;
     }
 
     public NavigationHelper goTo() {
